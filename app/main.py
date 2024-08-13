8d342f350603811b883b37e1e02520c044dc00c5
enansaif <enansaifme33@gmail.com>
2024-08-13 16:21:21 +0600

    send echo

--- app/main.py
+++ app/main.py
@@ -7,9 +7,12 @@ def main():
         client, _ = server_socket.accept() 
         request = client.recv(1024).decode("utf-8")
         status_lint = request.split("\r\n")[0]
-        target = status_lint.split(" ")[1].split("/")
-        if target[1] == "echo":
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(target[2])}\r\n\r\n{target[2]}"
+        target = status_lint.split(" ")[1]
+        if target == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif target[:6] == "/echo/":
+            value = target[6:]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}"
         else:
             response = f"HTTP/1.1 404 Not Found\r\n\r\n"
         client.send(response.encode())
