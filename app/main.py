d236b2755ac47fd9f0c542027883429109b87b3f
enansaif <enansaifme33@gmail.com>
2024-08-13 12:36:40 +0600

    send http ok message

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    while True:
+        conn, _ = server_socket.accept() # wait for client
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        conn.close()
 
 if __name__ == "__main__":
     main()
