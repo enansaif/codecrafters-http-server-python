ed609becfcc1f076c70528ec2f4a3bdc49c6a841
enansaif <enansaifme33@gmail.com>
2024-08-13 17:06:59 +0600

    user agent body

--- app/main.py
+++ app/main.py
@@ -6,10 +6,14 @@ def main():
     while True:
         client, _ = server_socket.accept() 
         request = client.recv(1024).decode("utf-8")
-        status_lint = request.split("\r\n")[0]
+        request_split = request.split("\r\n")
+        status_lint = request_split[0]
         target = status_lint.split(" ")[1]
         if target == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif target[:11] == "/user-agent":
+            value = request_split[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}"
         elif target[:6] == "/echo/":
             value = target[6:]
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}"
